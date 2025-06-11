9b81899ed5b3fef7022fad063fb9b733568b7e08
Gabriel Toledo de Faria <bielddefaria@gmail.com>
2025-06-11 17:22:22 -0300

    Add files via upload

---
+++ UC4 Atividades 2/atividades1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>Add commentMore actions
+
+int main(){
+    // Declaração de variaveis
+
+    int idade;
+    float valorDoPgto;
+    double velParticula;
+    char tipoHabMotor;
+
+    return 0;
+}
---
+++ UC4 Atividades 2/atividades10.c
@@ -0,0 +1,8 @@
+// Aritmetica de PonteiroAdd commentMore actions
+#include<stdio.h>
+int main(){
+    int array[5] = {1,2,3,4,5};
+    int *ponteiro = array;
+    printf("%d\n", *(ponteiro + 2));
+    return 0;
+    }
